9abc3af08fe4b2d5d0a719c81bd40438a551ee06
kamal-aldeen-mohamed <kamalaldeenmohamed@gmail.com>
2024-12-26 00:22:29 +0200

    finish str

--- src/main/java/com/exa/demotwo/configration/AppConfig.java
+++ src/main/java/com/exa/demotwo/configration/AppConfig.java
@@ -6,15 +6,13 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
-import org.springframework.jdbc.datasource.DriverManagerDataSource;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.thymeleaf.extras.springsecurity6.dialect.SpringSecurityDialect;
 
 @EnableWebMvc
 @Configuration
-@ComponentScan(basePackages = { "banquemisr.challenege05.taskmangmentsystem" })
+@ComponentScan(basePackages = { "com.exa.demotwo" })
 @PropertySource("classpath:application.properties")
 public class AppConfig {
     @Autowired
@@ -28,20 +26,8 @@ public class AppConfig {
         return ivr;
     }
 
-    @SuppressWarnings("null")
-    @Bean
-    DriverManagerDataSource getDataSource() {
-        DriverManagerDataSource dataSource = new DriverManagerDataSource();
-        dataSource.setDriverClassName(env.getProperty("spring.datasource.driver-class-name"));
-        dataSource.setUrl(env.getProperty("spring.datasource.url"));
-        dataSource.setUsername(env.getProperty("spring.datasource.username"));
-        dataSource.setPassword(env.getProperty("spring.datasource.password"));
-        return dataSource;
-    }
+  
 
-    @Bean
-    SpringSecurityDialect springSecurityDialect() {
-        return new SpringSecurityDialect();
-    }
+   
 
 }
